c4837c8d7072a9d402bd72e60a13993f2491feac
J-Sawicki <kubasaw98@gmail.com>
2022-02-23 20:22:09 +0100

    Add project files.

---
+++ ACME.BLTest/CustomerTest.cs
@@ -0,0 +1,79 @@
+﻿using ACME.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ACME.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "James",
+                LastName = "Delaney"
+            };
+            string expected = "Delaney, James";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Delaney"
+            };
+            string expected = "Delaney";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "James"
+            };
+            string expected = "James";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void StaticTest()
+        {
+            //-- Arrange
+            var c1 = new Customer();
+            Customer.InstanceCount++;
+
+            var c2 = new Customer();
+            Customer.InstanceCount++;
+            
+            var c3 = new Customer();
+            Customer.InstanceCount++;
+
+            //--Act
+
+            //--Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+    }
+}
---
+++ ACME.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACME.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACME.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6537e9f9-b15d-483e-917e-09a77c2ab615")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
